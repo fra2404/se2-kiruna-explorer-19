67696b8c0be53b3012a896fffb081656583c7828
Diego Porto <101588208+akhre@users.noreply.github.com>
2024-10-29 00:30:47 +0100

    Added simple map of Kiruna

---
+++ client/components/Map.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
+import { LatLngExpression } from 'leaflet';
+
+export default function KirunaMap() {
+    const position: LatLngExpression = [67.85572, 20.22513];   // this are DD coordinates for Kiruna different from DMS coordinates for Kiruna
+
+    useEffect(() => {
+        console.log('Map container rendered');
+    }, []);
+
+
+
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+      }
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+
+
+    return (
+        <div style={{ width: width, height: height}}>
+            <MapContainer
+                style={{ width: "100%", height: "100%" }} center={position} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
+        </div>
+    );
+}
