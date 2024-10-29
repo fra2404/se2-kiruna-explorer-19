17c448c1cc48505fd9d4f339cc27f93610696b90
Diego Porto <101588208+akhre@users.noreply.github.com>
2024-10-29 21:26:47 +0100

    Fixed typo

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,7 +1,5 @@
 import { Button } from "@material-tailwind/react";
 
-const { isLoggedIn, user } = useAuth();   // Retrieve this information from the context created by FAL
-
 
 export function ButtonRounded() {
     return (
