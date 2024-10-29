b0911191136cb6394e2caf3a6b529a73dfab3642
PAOLO CAGLIERO s324194 <s281559@studenti.polito.it>
2024-10-29 16:01:14 +0100

    Merging main into dev

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
+import { Route, Routes, useParams } from 'react-router-dom';
+import KirunaMap from './components/Map.tsx'
+import ComponentExample from './components/ComponentExample.tsx';
 
 import './App.css'
 
-
-import KirunaMap from './components/Map.tsx'
-
-
 function App() {
   return (
-    <>
-    <KirunaMap >
+    <Routes>
+      <Route path="/" element={
+        <KirunaMap >
 
-    </KirunaMap>
-    </>
+        </KirunaMap>
+      }/>
+      <Route path="/component-example" element={
+        <ComponentExample />
+      }/>
+    </Routes>
   )
 }
 
