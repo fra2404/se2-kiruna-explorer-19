f793b4dd14b60d788f636624cb7ee8ca58aff9e8
PAOLO CAGLIERO s324194 <s281559@studenti.polito.it>
2024-10-29 19:08:16 +0100

    Created hardcoded areas

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, Polygon, Popup, TileLayer } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
-import { LatLngExpression } from 'leaflet';
+import L, { LatLngExpression } from 'leaflet';
 
 
 export default function KirunaMap() {
@@ -11,7 +11,17 @@ export default function KirunaMap() {
         console.log('Map container rendered');
     }, []);
 
+    const areasCoords: Record<string, LatLngExpression[]> = {
+        "City center": [[67.854844, 20.243384], [67.849990, 20.243727], [67.850702, 20.266230], [67.857173, 20.265538]],
+        "Luossajarvi": [[67.862737, 20.186711], [67.868170, 20.166441], [67.877093, 20.165441], [67.874507, 20.186398], [67.866747, 20.198250]],
+        "Kiirunavaaragruvan": [[67.839309, 20.214946], [67.833351, 20.225252], [67.833092, 20.203952]]
+    };
 
+    const areas = Object.entries(areasCoords).map(([areaName,coords]) => {
+        return (<Polygon key={areaName} pathOptions={{color: "blue"}} positions={coords}>
+            <Popup>{areaName}</Popup>
+        </Polygon>)
+    });
 
     const [width, setWidth] = useState(window.innerWidth);
     const [height, setHeight] = useState(window.innerHeight);
@@ -47,6 +57,9 @@ export default function KirunaMap() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+                {
+                    areas
+                }
             </MapContainer>
         </div>
     );
