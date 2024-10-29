603d5214fcc1e00e0b49708f972040f2bd4797ae
Francesco <73170315+fra2404@users.noreply.github.com>
2024-10-29 11:23:38 +0100

    Refactor package.json to add "@jest/globals" dependency

--- server/unit_test/st1_unit_test.ts
+++ server/unit_test/st1_unit_test.ts
@@ -3,10 +3,11 @@ import { Request, Response, NextFunction } from 'express';
 
 //service -> controller -> route
 
-import { getAllUsers } from '@services/user.service';
-import { IUserResponse } from '@interfaces/user.return.interface';
 
 import { getUsers } from '../src/controllers/user.controllers';
+import { getAllUsers } from '../src/services/user.service';
+import { IUserResponse } from '../src/interfaces/user.return.interface';
+import { IUser } from '../src/interfaces/user.interface';
 
 jest.mock('./path/to/userService');
 
@@ -21,14 +22,14 @@ describe('getUsers', () => {
             //response mock check
             json: jest.fn(),
         };
-            //errors mock check
-            next = jest.fn();
+        //errors mock check
+        next = jest.fn();
     });
 
     it('should return all users', async () => {
         const mockUsers: IUserResponse[] = [
-        { name:'Francesco', surname:'Albano', password:'example1', email:'example1@mail.com', role:'visitor' },
-        { name:'Sergio', surname:'Cicero', password:'example2', email:'example2@mail.com', role:'resident' },
+            { name: 'Francesco', surname: 'Albano', password: 'example1', email: 'example1@mail.com', phone: undefined, role: 'visitor' },
+            { name: 'Sergio', surname: 'Cicero', password: 'example2', email: 'example2@mail.com', phone: undefined, role: 'resident' },
         ];
 
         (getAllUsers as jest.Mock).mockResolvedValue(mockUsers);
@@ -45,7 +46,7 @@ describe('getUsers', () => {
 
         await getUsers(req as Request, res as Response, next);
 
-        expect(res.json).not.toHaveBeenCalled(); 
+        expect(res.json).not.toHaveBeenCalled();
         expect(next).toHaveBeenCalledWith(error);
     });
 });
