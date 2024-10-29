1ddfb0c41049b50a9a9eba1cbf64ce4de66a29fb
mariamtelly <mariamtelly73@gmail.com>
2024-10-29 12:35:48 +0100

    Creation of the login form

---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,10 @@
+
+const LoginForm = () => {
+    return (
+        <div className="max-w-[550px] mx-auto my-16 bg-white border px-6 py-3 rounded">
+            <h1 className="text-2xl font-bold text-center">Login</h1>
+        </div>
+    );
+}
+
+export default LoginForm;
