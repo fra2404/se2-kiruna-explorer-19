fd332e029386293a59cd2b373d9b3501fe45d629
Francesco <73170315+fra2404@users.noreply.github.com>
2024-10-29 10:07:14 +0100

    Refactor client files, add Tailwind CSS configuration - created the input component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import InputComponent from './components/atoms/input/input';
+
+const options = [
+  { value: 'US', label: 'United States', code: 'US', prefix: '+1' },
+  { value: 'IT', label: 'Italy', code: 'IT', prefix: '+39' },
+  { value: 'FR', label: 'France', code: 'FR', prefix: '+33' },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState('');
+
+  const handleChange = (
+    event: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >,
+  ) => {
+    setValue(event.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full max-w-lg space-y-4">
+        <InputComponent
+          label="Email"
+          type="email"
+          value={value}
+          onChange={handleChange}
+          required
+        />
+        <InputComponent
+          label="Password"
+          type="password"
+          value={value}
+          onChange={handleChange}
+          required
+        />
+        <InputComponent
+          label="Text"
+          type="text"
+          value={value}
+          onChange={handleChange}
+          required
+        />
+        <InputComponent
+          label="Textarea"
+          type="textarea"
+          value={value}
+          onChange={handleChange}
+          required
+        />
+        <InputComponent
+          label="Select"
+          type="select"
+          value={value}
+          onChange={handleChange}
+          options={options}
+          required
+        />
+        <InputComponent
+          label="Radio"
+          type="radio"
+          value={value}
+          onChange={handleChange}
+          options={options}
+          required
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
