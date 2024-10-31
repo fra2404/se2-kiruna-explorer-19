f32d66c8b66643998633cddcd1fac053b7e2a257
Francesco <73170315+fra2404@users.noreply.github.com>
2024-10-31 22:34:21 +0100

    Implement user authentication and document management; add API functions, context provider, and Docker configurations.

--- server/src/routes/document.routes.ts
+++ server/src/routes/document.routes.ts
@@ -1,7 +1,7 @@
 import {
-    addDocument
-  } from '@controllers/document.controllers';
-import {validateAddDocument} from '@utils/validators/document.validator';
+  addDocument
+} from '@controllers/document.controllers';
+import { validateAddDocument } from '@utils/validators/document.validator';
 import { handleValidationErrors } from '@middlewares/validation.middleware';
 
 import express from 'express';
@@ -13,4 +13,4 @@ const router = express.Router();
 router.post('/add', authenticateUser, authorizeRoles('PLANNER', 'DEVELOPER'), validateAddDocument, handleValidationErrors, addDocument);
 
 
-export default router;
+export const documentRoutes = router;
