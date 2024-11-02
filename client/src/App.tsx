ba4567da723914eb5654ea4de9e8865284ca3ec6
mariamtelly <mariamtelly73@gmail.com>
2024-11-02 14:39:48 +0100

    small changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,52 +5,17 @@ import ComponentExample from './components/ComponentExample.tsx';
 import './App.css';
 import LoginForm from './components/LoginForm';
 import { AuthProvider } from './context/AuthContext.tsx';
-// import InputComponent from './components/atoms/input/input';
-
-// const options = [
-//   { value: 'US', label: 'United States', code: 'US', prefix: '+1' },
-//   { value: 'IT', label: 'Italy', code: 'IT', prefix: '+39' },
-//   { value: 'FR', label: 'France', code: 'FR', prefix: '+33' },
-// ];
-// const [value, setValue] = useState('');
-
-// const handleChange = (
-//   event: React.ChangeEvent<
-//     HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
-//   >,
-// ) => {
-//   setValue(event.target.value);
-// };
 
 function App() {
 
 
-  return (
-    /* 
+  return ( 
     <Routes>
-      <Route path="/" element={
-        <KirunaMap >
-
-        </KirunaMap>
-      }/>
-
-      <Route path="/component-example" element={
-        <ComponentExample />
-      }/>
-
-      <Route path="/login" element={
-        <LoginPage />
-      }/>
-
-      <Route path="/new-document" element={<DocumentCreationPage />} />
+      <Route path="/" element={<KirunaMap></KirunaMap>}/>
     </Routes>
-  ) */
-    <AuthProvider>
-      <div className="w-screen h-screen flex flex-col items-center justify-center">
-        <LoginForm />
-      </div>
-    </AuthProvider>
   );
 }
+
+
 
 export default App;
