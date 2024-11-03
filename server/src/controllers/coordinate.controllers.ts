7e76ed2f8a8a1bb2b03328f05e665a1e08889960
Francesco <73170315+fra2404@users.noreply.github.com>
2024-11-03 10:21:39 +0100

    Add getCoordinateById controller and validation for coordinate ID

--- server/src/controllers/coordinate.controllers.ts
+++ server/src/controllers/coordinate.controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
-import { addCoordinateService, getAllCoordinates } from '@services/coordinate.service';
+import { addCoordinateService, getAllCoordinates, getCoordinateById } from '@services/coordinate.service';
 import { CustomError } from '@utils/customError';
+import { PositionError } from '@utils/errors';
 
 export const addCoordinate = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
@@ -17,5 +18,19 @@ export const getAllCoordinatesController = async (req: Request, res: Response, n
         res.status(200).json(coordinates);
     } catch (error) {
         next(error);
+    }
+};
+
+export const getCoordinateByIdController = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const { id } = req.params;
+        const coordinate = await getCoordinateById(id);
+        if (!coordinate) {
+            console.log('Coordinate not found');
+            return next(new PositionError())
+        }
+        res.status(200).json(coordinate);
+    } catch (error) {
+        next(new CustomError('Internal Server Error', 500));
     }
 };
--- server/src/routes/coordinate.routes.ts
+++ server/src/routes/coordinate.routes.ts
@@ -2,13 +2,15 @@
 import express from 'express';
 import { authenticateUser } from '@middlewares/auth.middleware';
 import { authorizeRoles } from '@middlewares/role.middleware';
-import { validateCoordinate } from '@utils/validators/coordinate.validator';
+import { validateCoordinate, validateId } from '@utils/validators/coordinate.validator';
 import { handleValidationErrors } from '@middlewares/validation.middleware';
-import { addCoordinate, getAllCoordinatesController } from '@controllers/coordinate.controllers';
+import { addCoordinate, getAllCoordinatesController, getCoordinateByIdController } from '@controllers/coordinate.controllers';
 
 const router = express.Router();
 
 router.post('/create', authenticateUser, authorizeRoles('PLANNER', 'DEVELOPER'), validateCoordinate, handleValidationErrors, addCoordinate);
 router.get('/', getAllCoordinatesController);
+router.get('/:id', validateId, handleValidationErrors, getCoordinateByIdController);
+
 
 export const coordinateRoutes = router;
