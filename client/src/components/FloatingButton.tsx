729ac13b6499dcc2117699fc9e19175536d726db
Francesco <73170315+fra2404@users.noreply.github.com>
2024-11-05 18:52:28 +0100

    Enhance FloatingButton and Overlay components with hover effects and improved styling

--- client/src/components/FloatingButton.tsx
+++ client/src/components/FloatingButton.tsx
@@ -1,15 +1,31 @@
 import { ButtonRounded } from './Button';
 
 interface FloatingButtonProps {
-    onClick: () => void;
-    text: string;
+  className?: string;
+  onClick: () => void;
+  text: string;
+  onMouseEnter: () => void;
+  onMouseLeave: () => void;
 }
 
-export default function FloatingButton({ onClick, text }: FloatingButtonProps) {
-    return (
-        <div style={{ position: "fixed", top: "50%", right: "1rem", transform: "translateY(-50%)", zIndex: 1000 }}>
-            <ButtonRounded text={text} onClick={onClick} className="bg-black text-white text-base font-bold py-2 px-4 rounded-full">
-            </ButtonRounded>
-        </div>
-    );
-};
+export default function FloatingButton({
+  className = '',
+  onClick,
+  text,
+  onMouseEnter,
+  onMouseLeave,
+}: FloatingButtonProps) {
+  return (
+    <div
+      className={`floating-button ${className}`}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <ButtonRounded
+        text={text}
+        onClick={onClick}
+        className="bg-black text-white text-base font-bold py-2 px-4 rounded-full"
+      ></ButtonRounded>
+    </div>
+  );
+}
--- client/src/components/Overlay.tsx
+++ client/src/components/Overlay.tsx
@@ -6,39 +6,50 @@ import Modal from 'react-modal';
 import { modalStyles } from './Map';
 import DocumentForm from './DocumentForm';
 import FloatingButton from './FloatingButton';
+import { useState } from 'react';
+import './Overlay.css';
 
 interface OverlayProps {
-    coordinates: any        //Need to pass coordinates to the modal as parameter
+  coordinates: any; //Need to pass coordinates to the modal as parameter
 }
 
 export default function Overlay({ coordinates }: OverlayProps) {
-    const { modalOpen, setModalOpen } = useModal();
-    return (
-        <>
-            <Container fluid style={{ position: "absolute", top: "50vh", left: 0, width: "100%", zIndex: 1000 }}>
-                <FloatingButton text="+" onClick={() => {
-                    if (!modalOpen) {
-                        setModalOpen(true);
-                    }
-                }} >
-                </FloatingButton>
-                <Modal
-                    style={modalStyles}
-                    isOpen={modalOpen}
-                    onRequestClose={() => setModalOpen(false)}
-                >
-                    <DocumentForm
-                        coordinates={coordinates}
-                        modalOpen={modalOpen}
-                        setModalOpen={setModalOpen}
-                    />
-                </Modal>
-            </Container>
-
-        </>
-
-    )
-
-
+  const { modalOpen, setModalOpen } = useModal();
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <>
+      <Container
+        fluid
+        style={{
+          position: 'absolute',
+          top: '50vh',
+          left: 0,
+          width: '100%',
+          zIndex: 1000,
+        }}
+      >
+        <FloatingButton
+          text={isHovered ? '+ New Document' : '+'}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          onClick={() => {
+            if (!modalOpen) {
+              setModalOpen(true);
+            }
+          }}
+        />
+        <Modal
+          style={modalStyles}
+          isOpen={modalOpen}
+          onRequestClose={() => setModalOpen(false)}
+        >
+          <DocumentForm
+            coordinates={coordinates}
+            modalOpen={modalOpen}
+            setModalOpen={setModalOpen}
+          />
+        </Modal>
+      </Container>
+    </>
+  );
 }
-
