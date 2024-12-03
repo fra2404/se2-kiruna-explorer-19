cb9fb88e8195428708a579213820c5d02f69203e
Francesco <fransyalbano@gmail.com>
2024-12-03 23:59:18 +0100

    Updated coordinate schema to allow 'Polygon' type in addition to 'Point'

--- server/src/schemas/coordinate.schema.ts
+++ server/src/schemas/coordinate.schema.ts
@@ -11,7 +11,7 @@ const coordinateSchema = new Schema<ICoordinate>(
     type: {
       type: String,
       required: true,
-      enum: ['Point', 'Area'],
+      enum: ['Point', 'Polygon'],
     },
     coordinates: {
       type: Schema.Types.Mixed,
