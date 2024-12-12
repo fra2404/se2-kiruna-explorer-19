55cd37467d94b30c43e1c1a92950e8358bc8ab79
Francesco Albano - VM <fransyalbano@gmail.com>
2024-12-12 14:25:04 +0000

    Refactor stakeholder service and validator: streamline stakeholder retrieval, enhance error handling, and improve validation logic for new stakeholder types.

--- server/src/services/stakeholder.service.ts
+++ server/src/services/stakeholder.service.ts
@@ -6,34 +6,34 @@ import { StakeholderNotFoundError } from '@utils/errors';
 
 //Add new stakeholder
 export const addingStakeholder = async (
-    stakeholderData: any,
-  ): Promise<IStakeholder> => {
-    try {  
-      // Step 5: Save metadata along with the CDN output in DB
-      const stakeholder = new Stakeholder({
-        type: stakeholderData.newStakeholderType ,
-      });
-  
-      const NewStakeholder =  await stakeholder.save();
-      return NewStakeholder;
-    } catch (error) {
-      //console.error('Error in uploadMediaService:', error);
-      throw new CustomError('Internal Server Error', 500);
-    }
-  };
+  stakeholderData: any,
+): Promise<IStakeholder> => {
+  try {
+    // Step 5: Save metadata along with the CDN output in DB
+    const stakeholder = new Stakeholder({
+      type: stakeholderData.newStakeholderType,
+    });
+
+    const NewStakeholder = await stakeholder.save();
+    return NewStakeholder;
+  } catch (error) {
+    //console.error('Error in uploadMediaService:', error);
+    throw new CustomError('Internal Server Error', 500);
+  }
+};
 
 
-  // Get all stakeholders 
-  export const getAllStakeholders = async (): Promise<IStakeholder[]> => {
-    const Allstakeholders = await Stakeholder.find(); // Fetch all stakeholders
-  
-     // Not Found Stakeholder
-  if (Allstakeholders.length === 0) {
+// Get all stakeholders 
+export const getAllStakeholders = async (): Promise<IStakeholder[]> => {
+  const allStakeholders = await Stakeholder.find().select('-createdAt -updatedAt -__v'); // Fetch all stakeholders excluding specific fields
+
+  // Not Found Stakeholder
+  if (allStakeholders.length === 0) {
     throw new StakeholderNotFoundError();
   }
 
-       return Allstakeholders;
-  };
+  return allStakeholders;
+};
 
 
 // Get stakeholders by ID
@@ -41,9 +41,9 @@ export const getStakeholdersById = async (
   stakeholderId: string,
 ): Promise<IStakeholder | null> => {
   try {
-    const stakeholder = await Stakeholder.findById(stakeholderId); 
+    const stakeholder = await Stakeholder.findById(stakeholderId).select('-createdAt -updatedAt -__v');
     if (!stakeholder) {
-      throw new Error('Stakeholder not found'); 
+      throw new Error('Stakeholder not found');
     }
     return stakeholder.toObject();
   } catch (error) {
--- server/src/utils/validators/stakeholder.validator.ts
+++ server/src/utils/validators/stakeholder.validator.ts
@@ -6,22 +6,15 @@ export const validateNewStakeholderType = [
   body('newStakeholderType')
     .custom(async (value, { req }) => {
       const type = req.body.type;
-      
-     
-      if (type === 'New') {
-        if (!value || value.trim() === '') {   // If 'type' is 'New', 'newStakeholderType' cannotbe  empty.
-          throw new Error('New stakeholder type cannot be empty when "New" is selected.');
-        }
-         //check new stakeholder not exists in DB
-        const existingStakeholder = await Stakeholder.findOne({ type: { $regex: new RegExp(`^${value.trim()}$`, 'i') }, });
-        if (existingStakeholder) {
-          throw new Error('The stakeholder type already exists.');
-        }
-      } else {
-        // If 'type' is not 'New', 'newStakeholderType' should be empty (not shown at all).
-        if (value && value.trim() !== '') {
-          throw new Error('newStakeholderType should be empty if type is not "New".');
-        }
+
+
+      if (!value || value.trim() === '') {   // If 'type' is 'New', 'newStakeholderType' cannotbe  empty.
+        throw new Error('New stakeholder type cannot be empty when "New" is selected.');
+      }
+      //check new stakeholder not exists in DB
+      const existingStakeholder = await Stakeholder.findOne({ type: { $regex: new RegExp(`^${value.trim()}$`, 'i') }, });
+      if (existingStakeholder) {
+        throw new Error('The stakeholder type already exists.');
       }
       return true;
     }),
