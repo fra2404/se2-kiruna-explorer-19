60da9386c9b3abe392aeed67950d2a20f18d319d
Francesco <73170315+fra2404@users.noreply.github.com>
2024-12-23 05:29:43 +0100

    Update cookie settings for login and logout to allow cross-site usage

--- server/src/controllers/user.controllers.ts
+++ server/src/controllers/user.controllers.ts
@@ -201,8 +201,8 @@ export const login = async (
     const { token }: { token: string } = await loginUser(email, password);
     res.cookie('auth-token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: false,
+      sameSite: 'none',
       maxAge: 3600000, // 1 hour
       path: '/',
     });
@@ -287,8 +287,8 @@ export const logout = async (
   try {
     res.clearCookie('auth-token', {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: false,
+      sameSite: 'none',
       path: '/',
     });
     res.json({ message: 'User logged out successfully' });
