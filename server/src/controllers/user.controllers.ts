a2afac51227eefbc7c201a2ae6537912e6ce4661
Francesco <73170315+fra2404@users.noreply.github.com>
2024-12-23 11:58:32 +0100

    Set secure cookie flag to true for authentication token

--- server/src/controllers/user.controllers.ts
+++ server/src/controllers/user.controllers.ts
@@ -201,7 +201,7 @@ export const login = async (
     const { token }: { token: string } = await loginUser(email, password);
     res.cookie('auth-token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // Assicurati che il cookie sia inviato solo su connessioni HTTPS in produzione
+      secure: true,
       sameSite: 'none',
       domain: '.vercel.app',
       maxAge: 3600000, // 1 hour
